e925dd598bc63adad7ba689e92d3a9aac8bcdb64
Personal <ahmer.ali.ahsan@outlook.cpom>
2023-02-22 12:41:30 +0500

    Get started

---
+++ MortgageCadence/Controllers/WebhookController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MortgageCadenceWebhook.Controllers
+{
+    public class WebhookController : Controller
+    {
+        // GET: WebhookController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WebhookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WebhookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WebhookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WebhookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WebhookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WebhookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WebhookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostWebhook()
+        {
+            var json = "";
+            using (var inputStream = new System.IO.StreamReader(HttpContext.Request.Body))
+            {
+                json = await inputStream.ReadToEndAsync();
+            }
+            return Json(new { message = "success!" });
+        }
+    }
+}
